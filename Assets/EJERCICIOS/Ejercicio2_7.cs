708389857da6b0460a99bc4a9bdd96e8d5025749
JaimeLeonBueso <jaimeleonbueso@gmail.com>
2024-09-12 18:07:01 +0200

    Ejercicios clase terminados sin corregir

--- Assets/EJERCICIOS/Ejercicio2_7.cs
+++ Assets/EJERCICIOS/Ejercicio2_7.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class Ejercicio2_7 : MonoBehaviour
 {
+    float segundosTranscurridos = 14400;
+    float oroPorSegundo = 7;
     float oroAcumulado;
     float temporizador=4; //14.400segundos,240minutos, 4horas
 
-    void Update()
-    {
-        temporizador -= Time.deltaTime;
-        if (temporizador >= 0)
-        {
-            temporizador -= Time.deltaTime;
-            oroAcumulado = oroAcumulado + (7 * 3600 *Time.deltaTime);
-        }
-        if (temporizador < 0)
-        {
-            temporizador = 0;
-        }
-        if (temporizador == 0)
-        {
-            Debug.Log(oroAcumulado);
-        }
 
-       
+    void Start()
+    {
+        oroAcumulado = segundosTranscurridos * oroPorSegundo;
+        Debug.Log("El jugador ha acumulado "+oroAcumulado+ " unidades de oro en estas 4 horas");
+        
+
+
     }
+    //void Start()
+    //{
+    //    temporizador -= Time.deltaTime;
+    //    if (temporizador >= 0)
+    //    {
+    //        temporizador -= Time.deltaTime;
+    //        oroAcumulado = oroAcumulado + (7 * 3600 *Time.deltaTime);
+    //    }
+    //    if (temporizador < 0)
+    //    {
+    //        temporizador = 0;
+    //    }
+    //    if (temporizador == 0)
+    //    {
+    //        Debug.Log(oroAcumulado);
+    //    }
+
+
+    //}
 }
--- Assets/EJERCICIOS/Ejercicio2_8.cs
+++ Assets/EJERCICIOS/Ejercicio2_8.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class Ejercicio2_8 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float vida = 120;
+    float efectoveneno;
+    float turno;
     void Start()
     {
-        
+
+        efectoveneno = (120 / 100) * (3 * 1);
+        vida = vida - efectoveneno;
+        Debug.Log("Has sido atacado, te quedan " +vida+ " puntos de vida");
+
+        efectoveneno = (120 / 100) * (3 * 2);
+        vida = vida - efectoveneno;
+        Debug.Log("Has sido atacado, te quedan " + vida + " puntos de vida");
+
+        efectoveneno = (120 / 100) * (3 * 3);
+        vida = vida - efectoveneno;
+        Debug.Log("Has sido atacado, te quedan " + vida + " puntos de vida");
+
+        efectoveneno = (120 / 100) * (3 * 4);
+        vida = vida - efectoveneno;
+        Debug.Log("Has sido atacado, te quedan " + vida + " puntos de vida");
+
+        efectoveneno = (120 / 100) * (3 * 5);
+        vida = vida - efectoveneno;
+        Debug.Log("Has sido atacado, te quedan " + vida + " puntos de vida");
+
+
+
     }
 
     // Update is called once per frame
